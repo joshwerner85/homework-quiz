7f47f4d36fe9da2d8ea00f3729460841a23fbb2e
Josh <you@example.coexit>
2016-03-09 00:16:26 -0600

    final

---
+++ scripts/main.js
@@ -0,0 +1,217 @@
+ 
+ 
+// Question 1
+var question1 = console.log('Question 1: What does css stand for?' );
+var answer = prompt();
+
+if (answer === 'cascade style sheet') {
+    answer = answer
+    console.log('cascade style sheet: That is correct!');
+     
+}
+else {
+    console.log('Sorry that is wrong');
+     
+
+}
+// QUESTION 2
+var question2 = console.log('question 2: What does html stand for?');
+var answer = prompt();
+
+if (answer === 'hypertext markup language') {
+    answer = answer
+    console.log('hypertext markup language: That is correct!');
+     
+}
+else {
+    console.log('Sorry that is wrong');
+}	 
+
+
+// QUESTION 3
+var question3 = console.log('question 3: What is sass used for?');
+var answer = prompt();
+
+if (answer === 'preprocessor') {
+    answer = answer
+    console.log('preprocessor: That is correct!');
+     
+}
+else {
+    console.log('Sorry that is wrong');
+     
+}	 
+
+
+// QUESTION 4
+var question4 = console.log('question 4: Which property do you use to change a font?');
+var answer = prompt();
+
+if (answer === 'font-family') {
+    answer = answer
+    console.log('font-family: That is correct!');
+}
+
+else {
+    console.log('Sorry that is wrong');     
+}	
+
+
+// QUESTION 5
+var question5 = console.log('question 5: Where do you put the link tag for you css file?'); 
+var answer = prompt();
+
+if (answer === 'head tag') {
+    answer = answer
+    console.log('head tag: That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}
+
+// QUESTION 6
+var question6 = console.log('question 6: What is the first thing you type for an html file?'); 
+var answer = prompt();
+
+if (answer === '<!DOCTYPE html>') {
+    answer = answer
+    console.log('<!DOCTYPE html>: That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+// QUESTION 7
+var question7 = console.log('question 7: What is the next tag after <!DOCTYPE html>'); 
+var answer = prompt();
+
+if (answer === '<html>') {
+    answer = answer
+    console.log('<html>: That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+// QUESTION 8
+var question8 = console.log('question 8: Fill in the blank console._____'); 
+var answer = prompt();
+
+if (answer === 'log') {
+    answer = answer
+    console.log(' That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+// QUESTION 9
+var question9 = console.log('question 9: CSS is the_____ of a webpage'); 
+var answer = prompt();
+
+if (answer === 'styling') {
+    answer = answer
+    console.log(' That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+ 
+ // QUESTION 10
+var question10 = console.log('question 10: HTML is the_____ of a webpage'); 
+var answer = prompt();
+
+if (answer === 'structure') {
+    answer = answer
+    console.log(' That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+
+// QUESTION 11
+var question11 = console.log('question 11: Where does the title tag go?'); 
+var answer = prompt();
+
+if (answer === 'head tag') {
+    answer = answer
+    console.log(' That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+ 
+ // QUESTION 12
+var question12 = console.log('question 12: Where does the meta tag go?'); 
+var answer = prompt();
+
+if (answer === 'head') {
+    answer = answer
+    console.log(' That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+// QUESTION 13
+var question13 = console.log('question 13: Where is the best location for the <script> tag?'); 
+var answer = prompt();
+
+if (answer === 'body') {
+    answer = answer
+    console.log(' That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+
+// QUESTION 14
+var question14 = console.log('question 14: How do you write out a container as a class for a div? '); 
+var answer = prompt();
+
+if (answer === '.container') {
+    answer = answer
+    console.log(' That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
+
+ 
+ // QUESTION 15
+var question15 = console.log('question 15: What does prompt do?'); 
+var answer = prompt();
+
+if (answer === 'popup window') {
+    answer = answer
+    console.log(' That is correct!');
+     }
+
+else {
+    console.log('Sorry that is wrong');
+
+}   
