ab66fee0ce1099f0835da50af3679deb71528fe9
Josh <you@example.coexit>
2016-03-09 16:05:13 -0600

    Project complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,38 @@
- 
+ // answer++;
+ // answer +=1;
+ // answer = answer + 1;
+
+// = assignment answer = prompt()
+// 
+
+// 
+
+var correctAnswer = 0;
+var totalQuestions = 15;
+
  
 // Question 1
 var question1 = console.log('Question 1: What does css stand for?' );
 var answer = prompt();
 
 if (answer === 'cascade style sheet') {
-    answer = answer
+    correctAnswer++;
     console.log('cascade style sheet: That is correct!');
      
 }
 else {
+
     console.log('Sorry that is wrong');
      
 
 }
+
 // QUESTION 2
 var question2 = console.log('question 2: What does html stand for?');
 var answer = prompt();
 
 if (answer === 'hypertext markup language') {
-    answer = answer
+    correctAnswer++;
     console.log('hypertext markup language: That is correct!');
      
 }
@@ -33,7 +46,7 @@ var question3 = console.log('question 3: What is sass used for?');
 var answer = prompt();
 
 if (answer === 'preprocessor') {
-    answer = answer
+    correctAnswer++;
     console.log('preprocessor: That is correct!');
      
 }
@@ -48,7 +61,7 @@ var question4 = console.log('question 4: Which property do you use to change a f
 var answer = prompt();
 
 if (answer === 'font-family') {
-    answer = answer
+    correctAnswer++;
     console.log('font-family: That is correct!');
 }
 
@@ -62,7 +75,7 @@ var question5 = console.log('question 5: Where do you put the link tag for you c
 var answer = prompt();
 
 if (answer === 'head tag') {
-    answer = answer
+    correctAnswer++;
     console.log('head tag: That is correct!');
      }
 
@@ -76,7 +89,7 @@ var question6 = console.log('question 6: What is the first thing you type for an
 var answer = prompt();
 
 if (answer === '<!DOCTYPE html>') {
-    answer = answer
+    correctAnswer++;
     console.log('<!DOCTYPE html>: That is correct!');
      }
 
@@ -90,7 +103,7 @@ var question7 = console.log('question 7: What is the next tag after <!DOCTYPE ht
 var answer = prompt();
 
 if (answer === '<html>') {
-    answer = answer
+    correctAnswer++;
     console.log('<html>: That is correct!');
      }
 
@@ -104,7 +117,7 @@ var question8 = console.log('question 8: Fill in the blank console._____');
 var answer = prompt();
 
 if (answer === 'log') {
-    answer = answer
+    correctAnswer++;
     console.log(' That is correct!');
      }
 
@@ -118,7 +131,7 @@ var question9 = console.log('question 9: CSS is the_____ of a webpage');
 var answer = prompt();
 
 if (answer === 'styling') {
-    answer = answer
+    correctAnswer++;
     console.log(' That is correct!');
      }
 
@@ -133,7 +146,7 @@ var question10 = console.log('question 10: HTML is the_____ of a webpage');
 var answer = prompt();
 
 if (answer === 'structure') {
-    answer = answer
+    correctAnswer++;
     console.log(' That is correct!');
      }
 
@@ -148,7 +161,7 @@ var question11 = console.log('question 11: Where does the title tag go?');
 var answer = prompt();
 
 if (answer === 'head tag') {
-    answer = answer
+    correctAnswer++;
     console.log(' That is correct!');
      }
 
@@ -163,7 +176,7 @@ var question12 = console.log('question 12: Where does the meta tag go?');
 var answer = prompt();
 
 if (answer === 'head') {
-    answer = answer
+    correctAnswer++;
     console.log(' That is correct!');
      }
 
@@ -177,7 +190,7 @@ var question13 = console.log('question 13: Where is the best location for the <s
 var answer = prompt();
 
 if (answer === 'body') {
-    answer = answer
+    correctAnswer++;
     console.log(' That is correct!');
      }
 
@@ -192,7 +205,7 @@ var question14 = console.log('question 14: How do you write out a container as a
 var answer = prompt();
 
 if (answer === '.container') {
-    answer = answer
+    correctAnswer++;
     console.log(' That is correct!');
      }
 
@@ -207,7 +220,7 @@ var question15 = console.log('question 15: What does prompt do?');
 var answer = prompt();
 
 if (answer === 'popup window') {
-    answer = answer
+    correctAnswer++;
     console.log(' That is correct!');
      }
 
@@ -215,3 +228,13 @@ else {
     console.log('Sorry that is wrong');
 
 }   
+
+
+console.log(100 * correctAnswer/15);
+
+console.log(name+' you got ' +correctAnswer+ ' correct answers out of 15.');
+
+var results = correctAnswer /15;
+var score = results.toFixed(4) * 100 + '%'
+console.log('You scored a '+score+' on the quiz.');
+
